37923fd6a1fa3f9bc8ea2ec3359e8cc0e29f4e5a
ybkong <kyb0228@handong.ac.kr>
2023-09-16 17:19:11 +0900

    Final commit !

--- src/main/java/com/mycom/word/WordCRUD.java
+++ src/main/java/com/mycom/word/WordCRUD.java
@@ -87,7 +87,7 @@ public class WordCRUD implements ICRUD{
         System.out.println("-----------------------------");
     }
     public void updateItem() {
-        System.out.println("=> 수정할 단어 검색 : ");
+        System.out.print("=> 수정할 단어 검색 : ");
         String keyword = s.next();
         ArrayList<Integer> idlist = this.listAll(keyword);
         System.out.print("=> 수정할 번호 선택 : ");
@@ -98,7 +98,7 @@ public class WordCRUD implements ICRUD{
         String meaning = s.nextLine();
         Word word = list.get(idlist.get(id-1));
         word.setMeaning(meaning);
-        System.out.print("단어가 수정되었습니다. ");
+        System.out.println("단어가 수정되었습니다. ");
     }
 
     public void deleteItem() {
